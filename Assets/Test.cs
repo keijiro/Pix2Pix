e09b69433b73ab7d006af32cc7e23ca591b304c7
Keijiro Takahashi <keijiro@gmail.com>
2018-08-14 12:19:25 +0900

    benchmarking

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -12,12 +12,18 @@ namespace Pix2Pix
         {
             var weights = Path.Combine(Application.streamingAssetsPath, _weightFileName);
 
+            var stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Start();
+
             var result = ImageFilter.Deprocess(
                 Generator.Apply(
                     ImageFilter.Preprocess(_sourceTexture),
                     WeightReader.ReadFromFile(weights)
                 )
             );
+
+            stopwatch.Stop();
+            Debug.Log("Done. Total inference time is " + stopwatch.Elapsed);
 
             GetComponent<MeshRenderer>().material.mainTexture = result;
         }
